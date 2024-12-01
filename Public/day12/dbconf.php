fe3a1567186511f8e64cf275491c7b281104c873
hphls <139559763+hphls@users.noreply.github.com>
2024-12-01 14:54:22 +0000

    finish

---
+++ Public/day12/dbconf.php
@@ -0,0 +1,26 @@
+<?php
+    // gitignore is not working
+    /*
+    $servername = getenv('DB_HOST');
+    $username = getenv('DB_USER');
+    $password = getenv('DB_PASS');
+    $database = getenv('DB_NAME');
+    */
+
+    $servername = '127.0.0.1';
+    $username = 'root';
+    $password = 'mariadb';
+    $database = 'web';
+    try {
+        $connect = mysqli_connect($servername,$username,$password,$database);
+        if (!$connect) {
+            die("connection failed".mysqli_connect_error());
+        } 
+        else {
+            echo "Database is connected successfully" . "<br><br>";
+        }
+    } 
+    catch (Exception $e) {
+        die($e->getMessage());
+    }
+?>
